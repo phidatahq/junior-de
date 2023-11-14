176639d5e0e129e4267ff8fc283c920aa11e8d44
Ashpreet Bedi <ashpreet@phidata.com>
2023-11-14 23:31:18 +0000

    v2.0.46

--- duckgpt/s3/agent.py
+++ duckgpt/s3/agent.py
@@ -3,4 +3,7 @@ from phi.agent.duckdb import DuckDbAgent
 from workspace.settings import ws_settings
 
 duckdb_database = ws_settings.ws_root.joinpath("storage/s3.db")
-duckdb_agent = DuckDbAgent(db_path=str(duckdb_database))
+duckdb_agent = DuckDbAgent(
+    db_path=str(duckdb_database),
+    init_commands=["SET s3_region='us-east-1';"]
+)
